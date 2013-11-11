81dfdd8d47febdbbddf3b62cb1be8bfc55ed6c0c
Cony <Cony@Cony-PC>
2013-11-12 00:34:12 +0800

    bug fixing

--- src/main/java/com/papteco/client/action/FileActionUtils.java
+++ src/main/java/com/papteco/client/action/FileActionUtils.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class FileActionUtils {
 	
 	public static void openFile(String file) throws IOException{
-		Runtime.getRuntime().exec("cmd /C Start " + file); 
+		Runtime.getRuntime().exec("cmd /C Start \" \" \"" + file + "\""); 
 	}
 
 }
